7bc10a15ce595b7a4c901290f61955a1047b1aee
5solbin <dhrhfqls@naver.com>
2023-10-30 21:12:20 +0900

    first commit

---
+++ src/main/java/hospital/hongik/HongikApplication.java
@@ -0,0 +1,13 @@
+package hospital.hongik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongikApplication.class, args);
+	}
+
+}
